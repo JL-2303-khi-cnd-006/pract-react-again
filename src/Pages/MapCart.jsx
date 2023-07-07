9a24db65f7582be23483449598b0f2c6d968d4ff
Javeria Lodhi <javeria.lodhi@xloopdigital.com>
2023-07-06 19:06:32 -0700

    updated

--- src/Pages/MapCart.jsx
+++ src/Pages/MapCart.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Product } from '../Products'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Content/ShopContent'
 
 const MapCart = (props) => {
     const {id, title, description, price, image } = props.data
-
+    const {addToCart, removeFromCart, cartItems} = useContext(ShopContext)
+    const value = cartItems[id]
 
   return (
     <div class="card" style={{width: '18rem'}}>
@@ -13,9 +14,9 @@ const MapCart = (props) => {
         <p>${price}</p>
     </div>
     <div className='items'>
-    <button> - </button>
-    <input className='input'/>
-    <button> + </button>
+    <button onClick={(e) => removeFromCart(id)}> - </button>
+    <input className='input' value={value}/>
+    <button onClick={(e) => addToCart(id)}> + </button>
     </div>
     </div>
   )
